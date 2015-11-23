5da6c276df3ce273513bd44c2832f9f829ba09cd
ealwais <ealwais>
2015-11-23 08:42:43 -0600

    updating

---
+++ testagain/sublimeisbetter/metadata.rb
@@ -0,0 +1,7 @@
+name             'sublimeisbetter'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures sublimeisbetter'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
